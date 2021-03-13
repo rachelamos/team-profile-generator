6c6cd479e6a7b7a5e53d0e12932fb9dbd34aafa9
Rachel Amos <rachelamos35@gmail.com>
2021-03-13 16:07:03 -0600

    editing employee test file

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -4,21 +4,23 @@ const Employee = require("../employee");
 
 describe('Employee', () => {
     describe("Initialization", () => {
-        it("should return an object containing a 'name' property when called with the 'new' keyword", () => {
+        it("should return an object containing a 'name', 'id', and 'email' property when called with the 'new' keyword", () => {
             const obj = new Employee();
 
             expect("name" in obj).toEqual(true);
-        });
-        it("should return an object containing a 'id' property when called with the 'new' keyword", () => {
-            const obj = new Employee();
-
             expect("id" in obj).toEqual(true);
-        });
-        it("should return an object containing a 'email' property when called with the 'new' keyword", () => {
-            const obj = new Employee();
-
             expect("email" in obj).toEqual(true);
         });
     });
     
-})
+});
+
+test('can add name, id, and email using the constructor', () => {
+    const name = "Rachel";
+    const id = 12;
+    const email = "some@gmail.com";
+    const employee = new Employee("Rachel", 12, "some@gmail.com");
+    expect(employee.name).toBe(name);
+    expect(employee.id).toBe(id);
+    expect(employee.email).toBe(email);
+});
